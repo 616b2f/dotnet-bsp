bb22d50decc61a807ede1af0a5dba45a5bbaec7d
Alexej Kowalew <616b2f@gmail.com>
2025-02-15 16:08:18 +0100

    test: add possibility log depending on the LogLevel

--- test/ServerCallbacks.cs
+++ test/ServerCallbacks.cs
@@ -1,62 +1,82 @@
 using bsp4csharp.Protocol;
 using StreamJsonRpc;
 using Xunit.Abstractions;
+using Microsoft.Extensions.Logging;
 
 namespace test;
 
-public class ServerCallbacks(ITestOutputHelper testOutputHelper)
+public class ServerCallbacks(ITestOutputHelper testOutputHelper, LogLevel logLevel = LogLevel.Warning)
 {
     private readonly List<Diagnostic> _diagnosticsCollection = [];
     public IReadOnlyCollection<Diagnostic> Diagnostics => _diagnosticsCollection.AsReadOnly();
+
+    private readonly List<object> _taskNotificationsCollection = [];
+    public IReadOnlyCollection<object> TaskNotifications => _taskNotificationsCollection.AsReadOnly();
+
+    private readonly List<LogMessageParams> _logMessagesCollection = [];
+    public IReadOnlyCollection<LogMessageParams> LogMessages => _logMessagesCollection.AsReadOnly();
+
+    private readonly LogLevel _logLevel = logLevel;
 
     [JsonRpcMethod(Methods.BuildPublishDiagnostics, UseSingleObjectParameterDeserialization = true)]
     public Task BuildPublishDiagnosticsRecievedAsync(PublishDiagnosticsParams publishDiagnosticsParams)
     {
         _diagnosticsCollection.AddRange(publishDiagnosticsParams.Diagnostics);
-        foreach (var diagnostic in publishDiagnosticsParams.Diagnostics)
+        if (_logLevel >= LogLevel.Information)
         {
-            testOutputHelper.WriteLine(diagnostic.Message);
+            foreach (var diagnostic in publishDiagnosticsParams.Diagnostics)
+            {
+                testOutputHelper.WriteLine(diagnostic.Message);
+            }
         }
         return Task.CompletedTask;
     }
-
-    private readonly List<LogMessageParams> _logMessagesCollection = [];
-    public IReadOnlyCollection<LogMessageParams> LogMessages => _logMessagesCollection.AsReadOnly();
 
     [JsonRpcMethod(Methods.BuildLogMessage, UseSingleObjectParameterDeserialization = true)]
     public void BuildLogMessage(LogMessageParams logMessageParams)
     {
         _logMessagesCollection.Add(logMessageParams);
-        testOutputHelper.WriteLine(logMessageParams.Message);
+        if (_logLevel >= LogLevel.Information)
+        {
+            testOutputHelper.WriteLine(logMessageParams.Message);
+        }
     }
 
-    private readonly List<object> _taskNotificationsCollection = [];
-    public IReadOnlyCollection<object> TaskNotifications => _taskNotificationsCollection.AsReadOnly();
-
     [JsonRpcMethod(Methods.BuildTaskStart, UseSingleObjectParameterDeserialization = true)]
-    public void BuildTaskStart(TaskStartParams taskStartParams)
+    public Task BuildTaskStartAsync(TaskStartParams taskStartParams)
     {
         var message = string.Format("TaskStart: {0}", taskStartParams.Message);
-        testOutputHelper.WriteLine(message);
+        if (_logLevel >= LogLevel.Information)
+        {
+            testOutputHelper.WriteLine(message);
+        }
         _taskNotificationsCollection.Add(taskStartParams);
+        return Task.CompletedTask;
     }
 
     [JsonRpcMethod(Methods.BuildTaskProgress, UseSingleObjectParameterDeserialization = true)]
-    public void BuildTaskProgress(TaskProgressParams taskProgressParams)
+    public Task BuildTaskProgressAsync(TaskProgressParams taskProgressParams)
     {
         var message = string.Format("TaskProgress: {0}", taskProgressParams.Message);
-        testOutputHelper.WriteLine(message);
+        if (_logLevel >= LogLevel.Information)
+        {
+            testOutputHelper.WriteLine(message);
+        }
         _taskNotificationsCollection.Add(taskProgressParams);
+        return Task.CompletedTask;
     }
 
     [JsonRpcMethod(Methods.BuildTaskFinish, UseSingleObjectParameterDeserialization = true)]
-    public void BuildTaskFinish(TaskFinishParams taskFinishParams)
+    public Task BuildTaskFinishAsync(TaskFinishParams taskFinishParams)
     {
         var message = string.Format("TaskFinish: {0}", taskFinishParams.Message);
-        testOutputHelper.WriteLine(message);
+        if (_logLevel >= LogLevel.Information)
+        {
+            testOutputHelper.WriteLine(message);
+        }
         _taskNotificationsCollection.Add(taskFinishParams);
+        return Task.CompletedTask;
     }
-    // public const string BuildTaskFinish = "build/taskFinish";
 
     // public const string BuildShowMessage = "build/showMessage";
     // public const string BuildTargetDidChange = "buildTarget/didChange";
