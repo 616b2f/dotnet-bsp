9d4f924f07ff6ced0c1ad915e533aef6a6f7b264
Alexej Kowalew <616b2f@gmail.com>
2025-02-15 16:08:18 +0100

    test: add tests for TestCaseDiscovery request

---
+++ test-projects/xunit-tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿namespace xunit_tests;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("a")]
+    [InlineData("b")]
+    public void Test1_Success(string expectedValue)
+    {
+        Assert.Equal("a", expectedValue);
+    }
+
+    [Fact]
+    public void Test2()
+    {
+
+    }
+}
