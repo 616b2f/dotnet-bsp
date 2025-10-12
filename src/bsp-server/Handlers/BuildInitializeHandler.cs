0122a0f156643f944d1d5493250714ed88b6476f
Alexej Kowalew <616b2f@gmail.com>
2025-10-12 22:54:22 +0200

    feat: set options for MSBuildLocator

--- src/bsp-server/Handlers/BuildInitializeHandler.cs
+++ src/bsp-server/Handlers/BuildInitializeHandler.cs
@@ -1,4 +1,5 @@
-﻿using BaseProtocol;
+﻿using System.Text.Json;
+using BaseProtocol;
 using bsp4csharp.Protocol;
 using Microsoft.Build.Locator;
 
@@ -16,9 +17,34 @@ internal class BuildInitializeHandler(BuildInitializeManager initializeManager)
     {
         _initializeManager.SetInitializeParams(request);
 
+        if (MSBuildLocator.IsRegistered)
+        {
+            throw new ServerNotInitializedException("MSBuild instance already registered.");
+        }
+
         // this has to be loaded before we try to use any Microsoft.Build.* references
-        var msBuildInstance = MSBuildLocator.RegisterDefaults();
-        context.Logger.LogInformation("MSBuild instance used: {}", msBuildInstance.MSBuildPath);
+        var instanceQueryOptions = new VisualStudioInstanceQueryOptions
+        {
+            DiscoveryTypes = DiscoveryType.DotNetSdk,
+            WorkingDirectory = request.RootUri.AbsolutePath,
+        };
+
+        var msBuildInstances = MSBuildLocator.QueryVisualStudioInstances(instanceQueryOptions)
+            .OrderByDescending(x => x.Version);
+        var msBuildInstance = msBuildInstances
+            .FirstOrDefault();
+        foreach (var instance in msBuildInstances)
+        {
+            context.Logger.LogInformation("MSBuild instance found: {}", JsonSerializer.Serialize(instance));
+        }
+
+        if (msBuildInstance == null)
+        {
+            throw new ServerNotInitializedException("MSBuild instance could not be found on your system.");
+        }
+
+        MSBuildLocator.RegisterInstance(msBuildInstance);
+        context.Logger.LogInformation("MSBuild instance used: {}", JsonSerializer.Serialize(msBuildInstance));
 
         var serverCapabilities = _initializeManager.GetInitializeResult();
 
