67299504c0e9ddc4266f604479dd2ec3da178586
Alexej Kowalew <616b2f@gmail.com>
2025-10-13 21:57:01 +0200

    fix: build target sources paths in SourcesItem

--- src/bsp-server/Handlers/BuildTargetSourcesHandler.cs
+++ src/bsp-server/Handlers/BuildTargetSourcesHandler.cs
@@ -22,6 +22,7 @@ internal class BuildTargetSourcesHandler
         _initializeManager.EnsureInitialized();
 
         var items = new List<SourcesItem>();
+        // TODO: handling for .sln targets is not implemented
         foreach (var target in sourcesParams.Targets)
         {
             if (target.ToString().EndsWith(".csproj"))
@@ -30,17 +31,21 @@ internal class BuildTargetSourcesHandler
                 var proj = pcol.LoadProject(target.ToString());
                 var documents = proj.GetItems("Compile");
 
+                var rootDir = Path.GetDirectoryName(target.ToString());
+
                 var sources = new List<SourceItem>();
                 foreach (var document in documents)
                 {
+                    var path = Path.GetFullPath(Path.Combine("./", document.EvaluatedInclude), rootDir ?? "/");
+                    context.Logger.LogInformation("Document: {0}", path);
                     sources.Add(new SourceItem
                     {
-                        Uri = UriFixer.WithFileSchema(document.EvaluatedInclude),
+                        Uri = UriFixer.WithFileSchema(path),
                         Kind = SourceItemKind.File,
                         Generated = false
                     });
                 }
-                var rootDir = Path.GetDirectoryName(target.ToString());
+
                 Uri[] roots = (rootDir != null) ? [UriFixer.WithFileSchema(rootDir)] : [];
                 var sourcesItem = new SourcesItem
                 {
